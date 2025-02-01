eb5ebfb35aaece7e4eac1afa7e00b545b9bce5ec
hirano-climb <hirano.climb.22@gmail.com>
2025-02-01 19:35:58 +0900

    railway

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  url:process.env.DB_URL,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
